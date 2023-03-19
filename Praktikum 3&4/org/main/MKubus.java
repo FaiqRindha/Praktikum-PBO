77f0ba65f195f3e7bd5202ca7b818e63d6b7f0c3
FaiqRindha <100180165+FaiqRindha@users.noreply.github.com>
2023-03-19 22:22:14 +0700

    Add files via upload

---
+++ Praktikum 3&4/org/main/MKubus.java
@@ -0,0 +1,19 @@
+/**
+ * File : MPoligon.java 15/03/2023
+ * Penulis : Fa'iq Rindha Maulana
+ * Deskripsi : driver class untuk poligon dan kubus
+ */
+ 
+package org.main;
+
+import org.bangunruang.Kubus;
+import org.bangundatar.BujurSangkar;
+
+public class MKubus{
+	public static void main(String[] args){
+		BujurSangkar bujurSangkar = new BujurSangkar(4);
+		Kubus kubus = new Kubus(bujurSangkar);
+		System.out.println("Luas Permukaan Kubus dengan panjang sisi 4 satuan : "+kubus.hitungLuasAlas());
+		System.out.println("Volume Kubus dengan panjang sisi 4 satuan : "+kubus.hitungVolume());
+	}
+}
